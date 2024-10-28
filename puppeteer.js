bb8ff3e9bc7f513a9cfeece5a2fe94fd74d60a41
zlatodima <dmitriy.zlatov1@gmail.com>
2024-10-28 19:21:31 +0100

    Added product data parser

--- puppeteer.js
+++ puppeteer.js
@@ -38,4 +38,65 @@ const urls = [
     'https://www.outdoorsrlshop.it/catalogo/2928-arco-man-t-shirt.html'
 ];
 
+const currencyCodes = new Map([
+    ["$", "USD"],
+    ["€", "EUR"],
+    ["£", "GBP"],
+    ["¥", "JPY"],
+    ["CHF", "CHF"],
+    ["R$", "BRL"],
+    ["R", "ZAR"],
+    ["₹", "INR"],
+    ["₩", "KRW"],
+    ["kr", "SEK"],
+    ["zł", "PLN"],
+    ["$", "CAD"],
+    ["$", "AUD"],
+    ["$", "MXN"],
+    ["$", "SGD"],
+    ["$", "HKD"]
+]);
+
+const errorStatusCodes = new Set([404, 403]);
+
+function getCurrencyCodeBySign(currencySign) {
+    return currencyCodes.get(currencySign);
+}
+
+async function getProductData(page, url) {
+    const response = await page.goto(url, { waitUntil: 'networkidle0' });
+
+    if (errorStatusCodes.has(response.status())) throw new Error(response.statusText()); 
+    
+    const pageHTML = await page.evaluate(() => document.querySelector('*').outerHTML);
+    const $ = load(pageHTML);
+    const fullPriceWithCurrency = $('.upyPrezzoFinale').text();
+    const [currencySign, fullPrice] = fullPriceWithCurrency.split(" ");
+    const currencyCode = getCurrencyCodeBySign(currencySign);
+    const title = $('main section h1').text();
+    
+    return {
+        url,
+        fullPrice,
+        discountedPrice: fullPrice,
+        currency: currencyCode,
+        title
+    }
+}
+
 import puppeteer from 'puppeteer';
+import { load } from 'cheerio';
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    let productDataRecords = [];
+    for (let url of urls) {
+        let productData = await getProductData(page, url);
+        productDataRecords.push(productData);
+    }
+
+    console.log(productDataRecords)
+    await browser.close();
+})();
